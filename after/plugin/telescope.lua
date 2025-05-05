34d0690c38f10f23d7f2a31881c2c732bf755c4a
JP <pwnderpants@pwnderpants.com>
2025-05-05 11:05:52 -0400

    [chore] chaning grep input messaging

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,5 +3,5 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>gr', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string({ search = vim.fn.input("Search in files for: ") });
 end)
