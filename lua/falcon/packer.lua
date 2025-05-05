a5f6c7dc8bda64ec837f5f8b3894825484448481
JP <pwnderpants@pwnderpants.com>
2025-05-05 11:38:57 -0400

    [feat] adding barbecue winbar plugin

--- lua/falcon/packer.lua
+++ lua/falcon/packer.lua
@@ -66,4 +66,16 @@ return require('packer').startup(function(use)
         end,
         requires = {'nvim-tree/nvim-web-devicons'}
     }
+    use({
+        "utilyre/barbecue.nvim",
+        tag = "*",
+        requires = {
+            "SmiteshP/nvim-navic",
+            "nvim-tree/nvim-web-devicons", -- optional dependency
+        },
+        after = "nvim-web-devicons", -- keep this if you're using NvChad
+        config = function()
+            require("barbecue").setup()
+        end,
+    })
 end)
