400a636e72103b659eb823184ef183c6df419c1f
JP <pwnderpants@pwnderpants.com>
2025-05-30 18:35:40 -0400

    [feat] adding colorizer plugin

--- init.lua
+++ init.lua
@@ -31,6 +31,7 @@ require('lazy').setup({
     require 'plugins.autopairs',
     require 'plugins.debug',
     require 'plugins.which-key',
+    require 'plugins.colorizer',
 })
 
 vim.cmd[[colorscheme tokyonight-night]]
