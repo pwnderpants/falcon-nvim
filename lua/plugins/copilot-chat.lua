4158bb626e79cb193e1472546baf704acbc91420
JP <pwnderpants@pwnderpants.com>
2025-07-01 22:46:13 -0400

    [chore] fixing Copilot autocomplete issue

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,14 +1,22 @@
 return {
-  {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
-      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+        { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
+        { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
-      -- See Configuration section for options
+        -- See Configuration section for options
+        window = {
+            layout = 'vertical', -- 'vertical', 'horizontal', 'float', 'replace', or a function that returns the layout
+            width = 0.33, -- fractional width of parent, or absolute width in columns when > 1
+            height = 0.5, -- fractional height of parent, or absolute height in rows when > 1
+        },
+
+        mappings = {
+            complete = {
+                insert = '<C-j>',
+            }
+        }
     },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
 }
